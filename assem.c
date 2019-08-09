8adb48036b66a28244195ef590c6b89981cf3026
dordaniel0 <53654584+dordaniel0@users.noreply.github.com>
2019-08-09 07:59:24 +0300

    Update assem.c

--- assem.c
+++ assem.c
@@ -5,7 +5,7 @@
 
 int main(){
 	struct list *head_list = (struct list*)malloc(sizeof(struct list));
-	int pre_valid = prerun(head_list); /* making a struct of all the words in the file , without white noise, returning  */ 
+	int pre_valid = prerun(head_list); /* making a struct of all the words in the file , without white noise, returning 0 If there are any problems*/ 
 	symbol *head_sym = (symbol*)malloc(sizeof(symbol)); 
         data *head_dat = (data*)malloc(sizeof(data));
 	int circ1_valid;
@@ -14,7 +14,7 @@ int main(){
 		printf("syntax error stopping assembling\n");
 		return 0;
 	}
-	circ1_valid =circ1(head_list,head_sym,head_dat);
+	circ1_valid =circ1(head_list,head_sym,head_dat);/* Creating the symbol table , returning 0 If there are any problems */
 	if(!circ1_valid){
         	printf("circ1 error stopping assembling\n");
         	return 0;
@@ -23,7 +23,7 @@ int main(){
 		printf("%s\n",head_sym->name);
 		head_sym = head_sym->next;
 	}*/
- 	circ2_valid = circ2(head_list,head_dat,head_sym);
+ 	circ2_valid = circ2(head_list,head_dat,head_sym); /* creating output */
 	if(!circ2_valid){
 		remove_files();
         	printf("circ2 error stopping assembling\n");
